196264bb179f017e5c14d7cac7fa5c0e84c31530
IngridAkeida <ingridsanchesp@gmail.com>
2024-02-21 11:56:11 +0100

    fix - banner size

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,8 +3,8 @@ import banner from "../assets/1-big.png";
 
 export default function Banner() {
   return(
-    <div className="flex justify-center items-center h-96 bg-indigo-900 y-95 px-8">
-      <Image src={banner} alt="banner" className='object-fill object-center h-96 w-auto'/>
+    <div className="flex justify-center items-center h-54 bg-indigo-900 y-95 px-8">
+      <Image src={banner} alt="banner" className='object-fill object-center w-auto'/>
     </div>
   )
 }
