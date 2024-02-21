501fa6001fb98802589dab5823f7d911f07895f2
IngridAkeida <ingridsanchesp@gmail.com>
2024-02-21 13:02:26 +0100

    update style and performer

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,8 +3,12 @@ import banner from "../assets/1-big.png";
 
 export default function Banner() {
   return(
-    <div className="flex justify-center items-center h-54 bg-indigo-900 y-95 px-8">
-      <Image src={banner} alt="banner" className='object-fill object-center w-auto'/>
+    <div className="flex justify-center items-center h-54 bg-indigo-900 py-95 px-8">
+      <Image src={banner} alt="banner" className='sm:block hidden object-fill object-center w-auto'/>
+      <div className='sm:hidden flex flex-col items-center justify-center h-96 bg-gradient-to-r to-yellow-300 from-fuchsia-500  px-8 font-bold shadow-2xl'>
+      <h3 className='font-serif text-4xl '>Receive emails with tutorials, news, and tips to advance your career as a FrontEnd Developer</h3>
+      <h3 className='font-mono text-xl text-fuchsia-500 bg-fuchsia-50 p-2 mt-2 italic'>Limited time registration!</h3>
+      </div>
     </div>
   )
 }
--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -3,6 +3,12 @@ import benefity1 from "../assets/2-big.png";
 import benefity2 from "../assets/3-big.png";
 import benefity3 from "../assets/4-big.png";
 
+//small
+
+import benefity1Sm from "../assets/2.png";
+import benefity2Sm from "../assets/3.png";
+import benefity3Sm from "../assets/4.png";
+
 export default function Benefity(){
 
   return(
@@ -10,15 +16,18 @@ export default function Benefity(){
       <h3 className="text-2xl font-semibold mb-4 text-left">Benefits to become a member</h3>
       <div className='flex flex-col md:flex-row justify-around items-center mx-4 gap-4'>
         <div className='flex flex-row md:flex-col justify-center items-center text-center'>
-          <Image src={benefity1} alt="" className='object-cover object-center h-52 w-52 rounded-full shadow-2xl m-2'/>
+          <Image src={benefity1} alt="benefity 1" className='sm:block hidden object-cover object-center h-52 w-52 rounded-full shadow-2xl m-2'/>
+          <Image src={benefity1Sm} alt="benefity 1" className='block sm:hidden object-cover object-center h-52 w-52 rounded-full shadow-2xl m-2'/>
           <div className='w-52'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
         </div>
         <div className='flex flex-row md:flex-col justify-center items-center text-center'>
-          <Image src={benefity2} alt="" className='object-cover object-center h-52 w-52 rounded-full shadow-2xl m-2'/>
+          <Image src={benefity2} alt="benefity 2" className='sm:block hidden object-cover object-center h-52 w-52 rounded-full shadow-2xl m-2'/>
+          <Image src={benefity2Sm} alt="benefity 2" className='block sm:hidden object-cover object-center h-52 w-52 rounded-full shadow-2xl m-2'/>
           <div className='w-52'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
         </div>
         <div className='flex flex-row md:flex-col justify-center items-center text-center'>
-          <Image src={benefity3} alt="" className='object-cover object-center h-52 w-52 rounded-full shadow-2xl m-2'/>
+          <Image src={benefity3} alt="benefity 3" className='sm:block hidden object-cover object-center h-52 w-52 rounded-full shadow-2xl m-2'/>
+          <Image src={benefity3Sm} alt="benefity 3" className='block sm:hidden object-cover object-center h-52 w-52 rounded-full shadow-2xl m-2'/>
           <div className='w-52'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
       </div>
     </div>
