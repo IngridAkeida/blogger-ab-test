abdf9340cdaf3a974d39d95c9cefeebb8504b050
IngridAkeida <ingridsanchesp@gmail.com>
2024-02-21 20:58:02 +0100

    update commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,29 @@
 "use client"
-import Home from "@/page/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Main  from "@/components/Main";
+import { useEffect, useState } from "react";
 
 
 export default function Page() {
+  const [visitor, setVisitor] = useState(0);
+
+  useEffect(() => {
+
+    const randomVisitor = Math.floor(Math.random()*10);
+
+    console.log(randomVisitor);
+
+    if (randomVisitor >= 5) {
+      setVisitor(1);
+    }
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={ <Home /> } />
-      </Routes>
-    </BrowserRouter>
-    
+    <div className="max-w-6xl m-auto bg-fuchsia-900">
+        <Header />
+        <Main />
+        <Footer visitor={visitor}/>
+      </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import logo from "../assets/logo.png"
-import { Link } from "react-router-dom";
+import Link from 'next/link'
 import { useState } from "react";
 
 
@@ -14,7 +14,7 @@ export default function Header() {
   return (
     <nav className='flex items-center justify-between h-20 bg-gradient-to-r to-indigo-900 from-fuchsia-800 bg-opacity-20 px-8 text-fuchsia-200 font-bold shadow-2xl'>
       <div className='flex items-center'>
-        <Link to='/'>
+        <Link href='/'>
           <div>
             <Image src={logo} alt="logo" className='h-16 object-fill object-center w-auto bg-fuchsia-50 rounded-full'/>
           </div>
@@ -23,10 +23,10 @@ export default function Header() {
       <div className="hidden md:flex justify-between gap-4 ml-40">
         <ul className="flex">
           <li className="hover:bg-yellow-200 hover:text-fuchsia-800 px-4 rounded-sm">
-            <Link to="/#about">About us</Link>
+            <Link href="/#about">About us</Link>
           </li>
           <li className="hover:bg-yellow-200 hover:text-fuchsia-800 px-4 rounded-sm">
-            <Link to="/#member">Become a member</Link>
+            <Link href="/#member">Become a member</Link>
           </li>
         </ul>
       </div>
@@ -39,10 +39,10 @@ export default function Header() {
         <div className="md:hidden absolute top-20 right-8 bg-white p-4 rounded-lg shadow-md">
           <ul>
             <li className="hover:bg-yellow-200 hover:text-fuchsia-800 px-4 py-2 rounded-sm">
-              <Link to="/#about">About us</Link>
+              <Link href="/#about">About us</Link>
             </li>
             <li className="hover:bg-yellow-200 hover:text-fuchsia-800 px-4 py-2 rounded-sm">
-              <Link to="/#member">Become a member</Link>
+              <Link href="/#member">Become a member</Link>
             </li>
           </ul>
         </div>
